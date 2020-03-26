f5159501c5fc1eb02b04f6df2a89d22030117826
ferpsalles <fee_salles@hotmail.com>
2020-03-25 21:52:38 -0300

    Exercicios entrada de dados

---
+++ src/AreaCirculo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Locale;
+
+public class AreaCirculo {
+    public static void main (String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o raio do círculo: ");
+        double raio =sc.nextDouble();
+
+        double area = 3.14159 * Math.sqrt(raio);
+        System.out.println ("A area do circulo:" + area);
+
+        sc.close();
+
+    }
+}
