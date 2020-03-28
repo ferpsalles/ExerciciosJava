02bef855d26f2e06b139dbdb8e1e949e7eeda55a
ferpsalles <fee_salles@hotmail.com>
2020-03-27 22:17:14 -0300

    Estruturas de Repetição

---
+++ src/EstruturaWhile.java
@@ -0,0 +1,28 @@
+//Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+//cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+//menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma)
+
+import java.util.Scanner;
+public class EstruturaWhile {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println ("Digite o valor da coordenada x:");
+        int x =sc.nextInt();
+        System.out.println ("Digite o valor da coordenada y:");
+        int y=sc.nextInt();
+        while ( x!=0 && y!=0) {
+            if (x > 0 && y > 0) {
+                System.out.println("Os valores pertencem ao primeiro quadrante");
+            } else if (x < 0 && y >= 0) {
+                System.out.println("Os valores pertencem ao segundo quadrante");
+            }
+            if (x < 0 && y < 0) {
+                System.out.println("Os valores pertencem ao terceiro quadrante");
+            } else {
+                System.out.println("Os valores pertencem ao quarto quadrante");
+            }
+
+        }
+    }
+}
